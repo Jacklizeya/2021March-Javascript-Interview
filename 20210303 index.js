0536f4306e97eb38e3c81f13fff02ea9a4dccfcd
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2021-03-03 22:39:30 -0700

    Create 20210303 index.js

---
+++ 20210303 index.js
@@ -0,0 +1,22 @@
+const LinkedList = require('./LinkedList');
+
+const seasons = new LinkedList();
+
+
+seasons.addToHead('summer');
+seasons.addToHead('spring');
+seasons.addToHead('fall')
+seasons.addToHead("winter")
+
+
+
+
+
+
+//seasons.addToTail('fall');
+//seasons.addToTail('winter');
+
+
+//easons.removeHead();
+seasons.printList();
+
